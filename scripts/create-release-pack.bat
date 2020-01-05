b0d43f306ab2f3c5a6391d429f03e83e6cb76c62
Bartłomiej Zając <bartoss97@gmail.com>
2020-01-05 17:11:34 +0100

    Update release script

--- scripts/create-release-pack.bat
+++ scripts/create-release-pack.bat
@@ -4,7 +4,7 @@ REM First cmd argument should be version code; it will be appended at the end of
 pushd %~dp0\..\
 
 MSBuild particles-simulator.sln /p:Configuration=Release;Platform=x64 /v:m /nologo
-7z a particles-simulator-%1.zip bin/windows-Release-x86_64/**/*.exe bin/windows-Release-x86_64/**/*.lib particles-simulator/res/**/* run.bat
+7z a particles-simulator-%1.zip bin/windows-Release-x86_64/**/*.exe particles-simulator/res/**/* run.bat
 
 popd
 PAUSE
