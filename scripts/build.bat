fc984e3320b84967be842889b7be74bb7eabf300
Bartłomiej Zając <bartoss97@gmail.com>
2020-01-05 16:55:08 +0100

    Scripts comments fix

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,6 +1,8 @@
-REM Requires MSBuild.exe to be accessible in directories from PATH env variable
 @echo off
+REM Requires MSBuild.exe to be accessible in directories from PATH env variable
 pushd %~dp0\..\
+
 MSBuild particles-simulator.sln /p:Configuration=Release;Platform=x64 /v:n /nologo
+
 popd
 PAUSE
